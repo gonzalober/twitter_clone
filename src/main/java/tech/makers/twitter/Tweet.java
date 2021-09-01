23580dc1833aaf11ea078a9231e5eec0ab6e54fe
gonzalober <gonzaloberhouet@gmail.com>
2021-09-01 17:50:33 +0100

    timestamp added

--- src/main/java/tech/makers/twitter/Tweet.java
+++ src/main/java/tech/makers/twitter/Tweet.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Date;
 
 // This is a Spring Entity.
 // It represents a Model in MVC.
@@ -15,16 +16,18 @@ public class Tweet {
     @GeneratedValue(strategy= GenerationType.AUTO)
     private Long id;
     private String body;
+    private Date created_at;
 
     protected Tweet() {}
 
     public Tweet(String body) {
         this.body = body;
+        this.created_at = new Date();
     }
 
     @Override
     public String toString() {
-        return String.format("Tweet[id=%d, body='%s']", id, body);
+        return String.format("Tweet[id=%d, body='%s', created_at=%s]", id, body, created_at);
     }
 
     public Long getId() {
@@ -34,4 +37,6 @@ public class Tweet {
     public String getBody() {
         return body;
     }
+
+    public Date getCreated_at() { return created_at; }
 }
--- src/test/java/tech/makers/twitter/TweetTest.java
+++ src/test/java/tech/makers/twitter/TweetTest.java
@@ -5,16 +5,19 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TweetTest {
+
     @Test
+
     void testConstructs() {
         Tweet subject = new Tweet("Hello, world!");
         assertEquals("Hello, world!", subject.getBody());
         assertEquals(null, subject.getId());
+        assertEquals(null, subject.getCreated_at());
     }
 
     @Test
     void testToString() {
         Tweet subject = new Tweet("Hello, world!");
-        assertEquals("Tweet[id=null, body='Hello, world!']", subject.toString());
+        assertEquals("Tweet[id=null, body='Hello, world!', created_at= Wed Sep 01 17:18:40 BST 2021]", subject.toString());
     }
 }
